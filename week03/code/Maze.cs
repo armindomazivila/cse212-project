b8c277bf1d00a9d2641214504241be8a41ce7881
github_username_armindomazivila <armindo.mazivila1990@gmail.com>
2026-09-18 13:14:43 +0200

    Update the changes

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -25,14 +25,13 @@ public class Maze
         _mazeMap = mazeMap;
     }
 
-    // TODO Problem 4 - ADD YOUR CODE HERE
-    /// <summary>
-    /// Check to see if you can move left.  If you can, then move.  If you
-    /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
-    /// </summary>
     public void MoveLeft()
     {
-        // FILL IN CODE
+        if (!_mazeMap[(_currX, _currY)][0])
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
+        _currX--;
     }
 
     /// <summary>
@@ -41,8 +40,13 @@ public class Maze
     /// </summary>
     public void MoveRight()
     {
-        // FILL IN CODE
+        if (!_mazeMap[(_currX, _currY)][1])
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
+        _currX++;
     }
+
 
     /// <summary>
     /// Check to see if you can move up.  If you can, then move.  If you
@@ -50,8 +54,13 @@ public class Maze
     /// </summary>
     public void MoveUp()
     {
-        // FILL IN CODE
+        if (!_mazeMap[(_currX, _currY)][2])
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
+        _currY++;
     }
+
 
     /// <summary>
     /// Check to see if you can move down.  If you can, then move.  If you
@@ -59,8 +68,16 @@ public class Maze
     /// </summary>
     public void MoveDown()
     {
-        // FILL IN CODE
+        if (!_mazeMap[(_currX, _currY)][3])
+        {
+            throw new InvalidOperationException("Can't go that way!");
+        }
+        _currY--;
+
+
     }
+
+
 
     public string GetStatus()
     {
--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -21,8 +21,25 @@ public static class SetsAndMaps
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        var wordSet = new HashSet<string>(words);
+        var pairs = new List<string>();
+
+        foreach (string word in words)
+        {
+            if (word[0] == word[1]) // Skip words with identical letters
+                continue;
+
+            string reversed = $"{word[1]}{word[0]}";
+            if (wordSet.Contains(reversed))
+            {
+                if (string.Compare(word, reversed) < 0) // Ensure each pair is added only once
+                {
+                    pairs.Add($"{word} & {reversed}");
+                }
+            }
+
+        }
+        return pairs;
     }
 
     /// <summary>
@@ -42,7 +59,16 @@ public static class SetsAndMaps
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            string degree = fields[1];
+
+            if (degrees.ContainsKey(degree))
+            {
+                degrees[degree]++;
+            }
+            else
+            {
+                degrees[degree] = 1;
+            }
         }
 
         return degrees;
@@ -66,8 +92,31 @@ public static class SetsAndMaps
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        if (word1.Length != word2.Length)
+            return false;
+
+        var letters = new Dictionary<char, int>();
+
+        foreach (char letter in word1)
+        {
+            if (letters.ContainsKey(letter))
+                letters[letter]++;
+            else
+                letters[letter] = 1;
+        }
+
+        foreach (char letter in word2)
+        {
+            if (!letters.ContainsKey(letter))
+                return false;
+
+            letters[letter]--;
+
+            if (letters[letter] < 0)
+                return false;
+        }
+
+        return true;
     }
 
     /// <summary>
@@ -96,11 +145,12 @@ public static class SetsAndMaps
 
         var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
 
-        // TODO Problem 5:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
-        // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
-        // 3. Return an array of these string descriptions.
-        return [];
+        var earthquakes = new List<string>();
+        foreach (var feature in featureCollection.Features)
+        {
+            earthquakes.Add($"{feature.Properties.Place} - Magnitude: {feature.Properties.Mag}");
+        }
+
+        return earthquakes.ToArray();
     }
 }
